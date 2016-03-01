e85cc99d87bc5d5ce8b40466e40b5031bb48474a
anunez <arturonunez25@gmail.com>
2016-03-01 10:23:14 +0100

    tuesday morning

--- filter_test.py
+++ filter_test.py
@@ -8,48 +8,19 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 from astropy.convolution import Gaussian2DKernel, MexicanHat2DKernel, convolve
 import os
+import library
 plt.ion()
 # Create masked array
-def create_marray(image):
-    mask = np.zeros((len(image), len(image[0])), dtype=bool)
-    for j in range (len(image)):
-        for i in range (len(image[j])):
-            if math.isnan(image[j][i]):
-                continue
-            else:
-                mask[j][i] = True
-
-    # Create the masked array. 1-mask, be careful.
-    m_array = ma.masked_array(image, mask)
-    return m_array
 
 def main():
-            
-    # Variables for the filename
-    path = "/home/abeelen/Herschel/DDT_mustdo_5/PLCK_SZ_G004.5-19.6-1/"
-    filename = "OD1271_0x50012da8L_SpirePhotoLargeScan_PLCK_SZ_G004.5-19.6-1_destriped_PSW.fits"
-            
-    # Load the data
-    fits_data_uncut = fits.getdata(path+filename,"image")
-    fits_coverage_uncut = fits.getdata(path+filename,"coverage")
-    # Define cuts
-    ycenter = int(len(fits_data_uncut)/2)
-    xcenter = int(len(fits_data_uncut[0])/2)
-    cut = 70
-    # Cut array to focus on center
-    fits_data = fits_data_uncut[xcenter-cut:xcenter+cut,ycenter-cut:ycenter+cut]
-    fits_coverage = fits_coverage_uncut[xcenter-cut:xcenter+cut,ycenter-cut:ycenter+cut]
-    
-    # Create masked array from fits_data
-    m_array = create_marray(fits_data)
-    # Create masked array from fits_data maskin with coverage
-    mask_coverage = m_array.mask * fits_coverage
-    above = fits_coverage > 8
-    
-    image = (above * fits_data)
-    print "Lenghts ", len(above), len(above[0])
+    wavelength_range = sys.argv[1]
+    print wavelength_range
+    above, fits_data = library.get_data(wavelength_range)
+    image = above * fits_data
 
-            
+    # Create mask without NaN
+    m_array = library.create_marray(fits_data)
+         
     # Make the Mexican hat kernel and convolve
     # The values given to the kernels should be 1.7328, 2.3963, 3.5270
     # for S, M and L.
@@ -90,22 +61,7 @@ def main():
     print "Mean", mean_data, "\nMax", np.nanmax(fits_data),\
           "\nMin", np.nanmin(fits_data)
     
-    ## Plotting the histogram
-    print "Histogram part"
-    #mex_1d = m_mex.ravel()
-    #histo = mex_1d
-    #print histo2
-    #histo = histo2[~np.isnan(histo2)]
-    #indices = histo < 0
-    #histo[indices] = 0
-    #indices = histo > 0.004
-    #histo[indices] = 0
-    #print histo
-    #bin_values, bin_boundaries = np.histogram(mex_1d[~np.isnan(mex_1d)], 200)
-    #histo = m_mex.compressed()
-    print m_mex[1].compressed()
-    #plt.hist(m_mex.compressed(), 200)
-    #plt.show()
+    
 
 
     #ma.compressed()
