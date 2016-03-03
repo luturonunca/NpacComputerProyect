a1f9f56a5bef25c3b4ea7b5fde10a2a21ea460d0
anunez <arturonunez25@gmail.com>
2016-03-03 20:56:32 +0100

    ds;dlskg

--- multifile-locator.py
+++ multifile-locator.py
@@ -9,6 +9,7 @@ import os
 from library import *
 
 plt.ion()
+str_factor = (np.pi/180)**2
 def file_traveler(path):
     """
     iterates over directory on two levels and
@@ -22,7 +23,7 @@ def file_traveler(path):
             files_array.append([path+fn+"/"+files,files])
     return files_array
 def compute_size_field(x,y,angle):
-    size = x * y *(2*np.pi*(1-np.cos(np.radians(angle))))
+    size = x * y * (angle**2) * str_factor
     return size
 
 
@@ -83,17 +84,22 @@ def main():
     final_S = [0,0,0,0,0,0]
     final_M = [0,0,0,0,0,0]
     final_L = [0,0,0,0,0,0]
+    total_size_S = 0
+    total_size_M = 0
+    total_size_L = 0
     for j in range(len(files_array)):
         waverange =  files_array[j][1][-7]
         above, fits_data, header = get_data_path(waverange,files_array[j][0], header=True)
         image = above * fits_data
-        size_S = compute_size_field(len(image[0]),len(image),\
+        total_size_S += compute_size_field(len(image[0]),len(image),\
                                abs(header['CDELT1']))
-        size_M = compute_size_field(len(image[0]),len(image),\
+        print header['CDELT1']
+        total_size_M += compute_size_field(len(image[0]),len(image),\
                                abs(header['CDELT1']))
-        size_L = compute_size_field(len(image[0]),len(image),\
+        total_size_L += compute_size_field(len(image[0]),len(image),\
                                abs(header['CDELT1']))
-        print len(image),len(image[0]), abs(header['CDELT1'])
+        
+        #print len(image),len(image[0]), abs(header['CDELT1'])
         dictionary = get_positions(files_array[j])
         coordinates = dictionary['centroids']
         for i in range(0, len(coordinates)):
@@ -103,19 +109,51 @@ def main():
             if waverange == "S":
                 cont += 1
                 if np.isnan(flux[2])== True:
-                        continue
+                    continue
                 flux_array_S.append(flux[2])
             elif waverange == "M":
-                cont += 1                                                          
+                cont += 1      
+                if np.isnan(flux[2])== True:                                    
+                    continue
                 flux_array_M.append(flux[2])
 
             elif waverange == "L":                                                   
-                cont += 1                                                          
+                cont += 1           
+                if np.isnan(flux[2])== True:                                    
+                    continue
                 flux_array_L.append(flux[2])
-    binboundaries = [0.001,0.002, 0.0029,0.0051,0.0069,0.0111,0.0289,0.04,0.5] 
+    binboundaries = [0.02, 0.029,0.051,0.069,0.111,0.289,0.511] 
+    bincenter = [0.0238, 0.0375, 0.0589, 0.0859, 0.1662, 0.3741]
+    plt.close("all")
+    plt.xscale("log")
+    F_S,_ = np.histogram(flux_array_S, bins=binboundaries )
+    print bincenter
+    FF_S = []
+    FF_M = []
+    FF_L = []
+    for i in range(len(F_S)):
+            
+            print float(F_S[i]), bincenter[i]**2.5, total_size_S
+            FF_S.append(float( F_S[i])*(bincenter[i]**2.5)/float(total_size_S))  
+    plt.plot(bincenter, FF_S, 'g^', markersize=20)
+    
+    F_M, _ = np.histogram(flux_array_M, bins=binboundaries )                     
+    print bincenter                                                         
+    for i in range(len(F_M)):
+            print total_size_M
+            FF_M.append(float( F_M[i])*(bincenter[i]**2.5)/(total_size_M))
+    print FF_M        
+    plt.plot(bincenter, FF_M, 'r^', markersize=20)
 
-    plt.hist(flux_array_S, bins=binboundaries)
+
+    F_L,_ = np.histogram(flux_array_L, bins=binboundaries )                     
+    print bincenter                                                             
+    for i in range(len(F_L)):     
+            FF_L.append(float( F_L[i])*(bincenter[i]**2.5)/float(total_size_L)) 
+    plt.plot(bincenter, FF_L, 'b^', markersize=20)
+    
     plt.show()
+    print FF_S
     print final_S, final_M, final_L
 
     return 0
